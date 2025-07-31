17ceb1372653abe4849ac092703e9be2aa510a76
Jessica Ryan <jessicaryan0419@gmail.com>
2025-07-31 12:35:13 +0700

    fix: combo and score ui

--- game/combo_ui.gd
+++ game/combo_ui.gd
@@ -1,14 +1,1 @@
 extends CanvasLayer
-
-@export var score_manager: NodePath
-@onready var combo_label = $ComboLabel
-
-func _ready():
-	var score_manager = get_node(score_manager)
-	score_manager.combo_changed.connect(update_combo_label, score_manager.combo)
-
-func update_combo_label(new_combo: int):
-	if new_combo <= 1:
-		combo_label.text = ""
-	else:
-		combo_label.text = "Combo %d" % new_combo
--- game/game_ui.gd
+++ game/game_ui.gd
@@ -1,11 +1,28 @@
-extends CanvasLayer
+extends Control
 
-@export var score_manager: NodePath
 @onready var score_label = $ScoreLabel
+@onready var combo_label = $ComboLabel
+@onready var score_manager = get_node("/root/Game/ScoreManager")
+
+var fade_tween: Tween
 
 func _ready():
-	var score_manager = get_node(score_manager)
 	score_manager.score_changed.connect(update_score_label, score_manager.score)
+	score_manager.combo_changed.connect(update_combo_label, score_manager.combo)
 
 func update_score_label(new_score: int):
 	score_label.text = "Score: %d" % new_score
+	
+func update_combo_label(new_combo: int):
+	if new_combo <= 1:
+		combo_label.text = ""
+	else:
+		combo_label.text = "Combo %d" % new_combo
+		combo_label.modulate.a = 1.0
+
+		if fade_tween and fade_tween.is_running():
+			fade_tween.kill()
+
+		fade_tween = create_tween()
+		fade_tween.EASE_IN_OUT
+		fade_tween.tween_property(combo_label, "modulate:a", 0.0, score_manager.combo_interval)
