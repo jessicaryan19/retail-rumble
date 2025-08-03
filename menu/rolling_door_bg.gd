ba31cde905b09619b52c52cc4c569cce602b0a3a
Jessica Ryan <jessicaryan0419@gmail.com>
2025-08-03 18:45:39 +0700

    feat: add sound to door and button

--- menu/rolling_door_bg.gd
+++ menu/rolling_door_bg.gd
@@ -2,14 +2,17 @@ extends Control
 
 signal transition_finished()
 @onready var animation_player = $AnimationPlayer
+@onready var sfx_roll_up: AudioStream = preload("res://sfx/Blinds_Opening.mp3")
+@onready var sfx_roll_down: AudioStream = preload("res://sfx/Kitchen-Drawer-4-Open_with_Silverware.mp3")
 
 func roll_up():
 	animation_player.play("roll_up")
+	AudioHandler.play_sfx(sfx_roll_up)
 	await animation_player.animation_finished
 	emit_signal("transition_finished")
 
 func roll_down():
-	print("in roll dwn")
 	animation_player.play("roll_down")
+	AudioHandler.play_sfx(sfx_roll_down)
 	await animation_player.animation_finished
 	emit_signal("transition_finished")
