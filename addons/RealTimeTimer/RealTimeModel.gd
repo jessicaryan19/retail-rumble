6dca0840464a847c2fdce5b389d4fb7f8cc42577
vanssnn <vanssnn@gmail.com>
2025-08-03 23:06:04 +0700

    feat: stopwatch

--- addons/RealTimeTimer/RealTimeModel.gd
+++
@@ -1,8 +0,0 @@
-class_name RealTimeModel
-
-var isPaused : bool = false
-var startTimeUnix : int = 0 # Start Date in unix of the timer
-var pauseTimeUnix : int = 0
-var pauseTimeDuration : int = 0
-var timerDuration : int = 0
-var interval : bool = false # If should interval
